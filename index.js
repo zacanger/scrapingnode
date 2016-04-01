00e1fa2b35656da06a2db88a80aa5ff17e336b77
zacanger <zac@zacanger.com>
2016-03-31 22:09:44 -0600

    :radio_button: version bump

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
 
-var ineed = require('ineed')
-  , url   = process.argv[2]
+'use strict'
 
-ineed
+const
+  ineed = require('ineed')
+, url   = process.argv[2]
+
+if (!url) {
+  process.stdout.write('\nplease supply a url! usage: scrapingnode npmjs.com/package/scapingnode\n\n')
+} else {
+  ineed
   .collect
   .images
   .hyperlinks
   .scripts
   .stylesheets
-  .from('http://'+url, function(err, response, result){
-  console.log(result)
-})
-
+  .from('http://' + url, (err, response, result) => {
+    process.stdout.write('\n' + JSON.stringify(result, null, 2) + '\n\n')
+  })
+}
